924e757074309084973d7cb77302745feabfeee6
pete3249 <katelynrpeterson16@gmail.com>
2021-03-02 12:27:51 -0500

    commit tree b36da4767d1c4e55f0d53ac9f70b8b50480b9259

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -1,13 +1,49 @@
 import React, { Component } from 'react'
+import {connect} from 'react-redux';
 
 class CreateTodo extends Component {
+  constructor() {
+    super();
+    this.state = {
+      text: '',
+    };
+  }
+  
+  handleChange = event => {
+    this.setState({
+      text: event.target.value
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.addTodo(this.state);
+    this.setState({
+      text: '',
+    });
+  };
+
   render() {
     return(
       <div>
-        Create Todo Component
-      </div>
+      <form onSubmit={this.handleSubmit}>
+        <p>
+          <label>add todo</label>
+          <input type="text" value={this.state.text} onChange={this.handleChange}/>
+        </p>
+        <input type="submit" />
+      </form>
+    </div>
     )
   }
 }
 
-export default CreateTodo;
+const mapDispatchToProps = dispatch => {
+  return {
+    addTodo: formData => dispatch({ type: 'ADD_TODO', payload: formData })
+  }
+}
+
+// do not need mapStateToProps because component doesn't need any state
+// Only need to dispatch an action here
+export default connect(null, mapDispatchToProps)(CreateTodo);
